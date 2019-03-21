bc51b38eadb30af12fa821620b94d10b03584efc
jho0078 <jho0078@gmail.com>
2019-03-21 07:48:23 +0000

    04_customize form by formhelper

--- boards/forms.py
+++ boards/forms.py
@@ -1,5 +1,8 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 from .models import Board
+
 
 # class BoardForm(forms.Form):
 #     title = forms.CharField(label='제목', widget=forms.TextInput(attrs={
@@ -38,3 +41,8 @@ class BoardForm(forms.ModelForm):
                             },
                         }
                                     
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'POST'
+        self.helper.add_input(Submit('submit', '작성!'))
