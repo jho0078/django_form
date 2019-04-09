9ef7aca43d29f1f6abcc401d17264bd2c39392e8
jho0078 <jho0078@gmail.com>
2019-04-09 05:50:05 +0000

    07_make auth_form.html

--- boards/views.py
+++ boards/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import Board
 from .forms import BoardForm
 
@@ -9,7 +10,8 @@ def index(request):
         'boards' : boards
     }
     return render(request, 'boards/index.html', context)
-    
+
+@login_required
 def create(request):
     # POST 요청이면 FORM 데이터를 처리한다.
     if request.method == 'POST':
@@ -41,7 +43,7 @@ def delete(request, board_pk):
     else:
         return redirect('boards:detail', board.pk)
         
-
+@login_required
 def update(request, board_pk):
     board = get_object_or_404(Board, pk=board_pk)
     if request.method == 'POST':
